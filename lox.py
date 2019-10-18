c0332fb6fb904f78e453167e73a5b3b032d47d3a
Lisa Leung <lileung0@gmail.com>
2019-10-18 15:47:42 -0700

    test: scanner (#2)

--- lox.py
+++ lox.py
@@ -1,22 +1,33 @@
+"""
+lox.lox
+~~~~~~~~~~~~~~~~
+The main file for the lox programming language interpreter
+"""
 import sys
 from scanner import Scanner
 
-class Lox():
+
+class Lox:
+    """ Lox class"""
     def __init__(self):
         self.had_error = False
         self.had_runtime_error = False
 
     def run_file(self, file):
-        with open(file) as f:
-            content = f.read()
+        """ Runs file
+        :param file: input file
+        """
+        with open(file) as content:
+            program = content.read()
 
-        self.run(content)
+        self.run(program)
         if self.had_error:
             sys.exit(65)
         elif self.had_runtime_error:
             sys.exit(70)
 
     def run_prompt(self):
+        """ Runs prompt """
         print("Running prompt")
         while True:
             line = input(">> ")
@@ -24,21 +35,34 @@ class Lox():
             # If we had an error, we should reset at new prompt
             self.had_error = False
             self.had_runtime_error = False
-
-    def run(self, line):
+    @staticmethod
+    def run(line):
+        """ Runs the input
+        :param line: line to tokenize and eval
+        """
         scanner = Scanner(line)
         scanner.scan_tokens()
         #list tokens here, loop through token and print them out
 
     def error(self, line, message):
+        """ error
+        :param line: the location of error
+        :param message: error message
+        """
         self.report(line + " " + message)
 
-    def report(self, line, where, message):
+    def report(self, line, where=None, message=None):
+        """ error
+        :param line: line of error
+        :param where: location of error
+        :param: message: error message
+        """
         self.had_error = True
-        raise Exception("[Line {0}] Error {1}: {2} ", line, where, message)
+        raise Exception("[Line {0}] Error {1}: {2} ".format(line, where, message))
 
 
 def main():
+    """ Main """
     lox = Lox()
     print(sys.argv)
     if len(sys.argv) > 2:
---
+++ tests/test_scanner.py
@@ -0,0 +1,114 @@
+from scanner import Scanner, TokenType
+
+
+class TestScannerStatement:
+    def test_statement(self):
+        line = "var language = lox;"
+        scanner = Scanner(line)
+        scanner.scan_tokens()
+        assert scanner._source == "var language = lox;"
+        assert scanner.tokens[0].__dict__['lexeme'] == 'var'
+        assert scanner.tokens[1].__dict__.get('lexeme') == ''
+        assert scanner.tokens[2].__dict__.get('lexeme') == ''
+        assert scanner.tokens[3].__dict__.get('lexeme') == 'language'
+        assert scanner.tokens[4].__dict__.get('lexeme') == ''
+        assert scanner.tokens[5].__dict__.get('lexeme') == ''
+        assert scanner.tokens[6].__dict__.get('lexeme') == '='
+        assert scanner.tokens[7].__dict__.get('lexeme') == ''
+        assert scanner.tokens[8].__dict__.get('lexeme') == ''
+        assert scanner.tokens[9].__dict__.get('lexeme') == 'lox'
+        assert scanner.tokens[10].__dict__.get('lexeme') == ''
+        assert scanner.tokens[11].__dict__.get('lexeme') == ';'
+        assert scanner.tokens[10].__dict__.get('lexeme') == ''
+
+
+class TestScannerSingleChars:
+    def test_parens(self):
+        line = "()"
+        scanner = Scanner(line)
+        scanner.scan_tokens()
+        assert scanner.tokens[0].__dict__['lexeme'] == '('
+        assert scanner.tokens[0].__dict__['type'] == TokenType.LEFT_PAREN
+        assert scanner.tokens[1].__dict__['lexeme'] == ''
+        assert scanner.tokens[1].__dict__['type'] == TokenType.EOF
+        assert scanner.tokens[2].__dict__['lexeme'] == ')'
+        assert scanner.tokens[2].__dict__['type'] == TokenType.RIGHT_PAREN
+
+    def test_braces(self):
+        line = "{}"
+        scanner = Scanner(line)
+        scanner.scan_tokens()
+        print(scanner)
+        assert scanner.tokens[0].__dict__['lexeme'] == '{'
+        assert scanner.tokens[0].__dict__['type'] == TokenType.LEFT_BRACE
+        assert scanner.tokens[1].__dict__['lexeme'] == ''
+        assert scanner.tokens[1].__dict__['type'] == TokenType.EOF
+        assert scanner.tokens[2].__dict__['lexeme'] == '}'
+        assert scanner.tokens[2].__dict__['type'] == TokenType.RIGHT_BRACE
+
+    def test_comma(self):
+        line = ","
+        scanner = Scanner(line)
+        scanner.scan_tokens()
+        print(scanner)
+        assert scanner.tokens[0].__dict__['lexeme'] == ','
+        assert scanner.tokens[0].__dict__['type'] == TokenType.COMMA
+        assert scanner.tokens[1].__dict__['lexeme'] == ''
+        assert scanner.tokens[1].__dict__['type'] == TokenType.EOF
+
+    def test_dot(self):
+        line = "."
+        scanner = Scanner(line)
+        scanner.scan_tokens()
+        print(scanner)
+        assert scanner.tokens[0].__dict__['lexeme'] == '.'
+        assert scanner.tokens[0].__dict__['type'] == TokenType.DOT
+        assert scanner.tokens[1].__dict__['lexeme'] == ''
+        assert scanner.tokens[1].__dict__['type'] == TokenType.EOF
+
+    def test_minus(self):
+        line = "-"
+        scanner = Scanner(line)
+        scanner.scan_tokens()
+        print(scanner)
+        assert scanner.tokens[0].__dict__['lexeme'] == '-'
+        assert scanner.tokens[0].__dict__['type'] == TokenType.MINUS
+        assert scanner.tokens[1].__dict__['lexeme'] == ''
+        assert scanner.tokens[1].__dict__['type'] == TokenType.EOF
+
+    def test_plus(self):
+        line = "+"
+        scanner = Scanner(line)
+        scanner.scan_tokens()
+        print(scanner)
+        assert scanner.tokens[0].__dict__['lexeme'] == '+'
+        assert scanner.tokens[0].__dict__['type'] == TokenType.PLUS
+        assert scanner.tokens[1].__dict__['lexeme'] == ''
+        assert scanner.tokens[1].__dict__['type'] == TokenType.EOF
+
+    def test_semicolon(self):
+        line = ';'
+        scanner = Scanner(line)
+        scanner.scan_tokens()
+        assert scanner.tokens[0].__dict__['lexeme'] == ';'
+        assert scanner.tokens[0].__dict__['type'] == TokenType.SEMICOLON
+        assert scanner.tokens[1].__dict__['lexeme'] == ''
+        assert scanner.tokens[1].__dict__['type'] == TokenType.EOF
+
+    def test_slash(self):
+        line = '/'
+        scanner = Scanner(line)
+        scanner.scan_tokens()
+        assert scanner.tokens[0].__dict__['lexeme'] == '/'
+        assert scanner.tokens[0].__dict__['type'] == TokenType.SLASH
+        assert scanner.tokens[1].__dict__['lexeme'] == ''
+        assert scanner.tokens[1].__dict__['type'] == TokenType.EOF
+
+    def test_star(self):
+        line = '*'
+        scanner = Scanner(line)
+        scanner.scan_tokens()
+        assert scanner.tokens[0].__dict__['lexeme'] == '*'
+        assert scanner.tokens[0].__dict__['type'] == TokenType.STAR
+        assert scanner.tokens[1].__dict__['lexeme'] == ''
+        assert scanner.tokens[1].__dict__['type'] == TokenType.EOF
